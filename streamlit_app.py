d002e6c567de868c856e7f547b5ba653ebb96333
eleonoraferns <140112509+eleonoraferns@users.noreply.github.com>
2023-11-05 05:44:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 
-st.text_input('Input', 'My')
+user_prompt = st.text_input("Enter your prompt:", value="Type your question here")
+
 st.altair_chart(alt.Chart(df, height=700, width=700)
     .mark_point(filled=True)
     .encode(
